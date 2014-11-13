f0ae04c3c3aaf4d400f03b7c107eb5be10a32dda
Pablo Toledo <jptgjuanpablo@gmail.com>
2014-11-13 20:42:05 +0100

    Update MisMezclas.java

--- Coctelera/src/dse/coctelera/MisMezclas.java
+++ Coctelera/src/dse/coctelera/MisMezclas.java
@@ -1,3 +1,20 @@
+
+/*
+ * Copyright (C) 2014 Drinkuino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dse.coctelera;
 
 import java.io.EOFException;
@@ -24,10 +41,10 @@ public class MisMezclas extends ListActivity {
 	private ListView listView = null;
 	private ListAdapter listAdapter = null;	
 	
-	//CÛdigos para identificar el tipo de actividad creada desde esta actividad
-	private int CODIGO_NUEVA = 0; //CÛdigo para la actividad de creaciÛn de una nueva mezcla
-	private int CODIGO_DETALLES = 1; //CÛdigo para la actividad de mostrar detalles de una mezcla
-	private int CODIGO_PREFERENCIAS = 2; //CÛdigo para la actividad de mostrar preferencias
+	//C√≥digos para identificar el tipo de actividad creada desde esta actividad
+	private int CODIGO_NUEVA = 0; //C√≥digo para la actividad de creaci√≥n de una nueva mezcla
+	private int CODIGO_DETALLES = 1; //C√≥digo para la actividad de mostrar detalles de una mezcla
+	private int CODIGO_PREFERENCIAS = 2; //C√≥digo para la actividad de mostrar preferencias
 	
 	public final static String EXTRA_MEZCLA_LISTADA = "dese.coctelera.MEZCLA_LISTADA";
 	public final static String EXTRA_POSICION_MEZCLA_LISTADA = "dese.coctelera.POSICION_MEZCLA_LISTADA";
@@ -39,22 +56,22 @@ public class MisMezclas extends ListActivity {
 		List<Mezcla> lista_mezclas = new ArrayList<Mezcla>();
 		
 		try {
-			fileStream = openFileInput(getString(R.string.FILENAME_MEZCLAS)); //Se abre el fichero donde se guardan las mezclas que se encuentra en el directorio de fichero de la aplicaciÛn
+			fileStream = openFileInput(getString(R.string.FILENAME_MEZCLAS)); //Se abre el fichero donde se guardan las mezclas que se encuentra en el directorio de fichero de la aplicaci√≥n
 			objectStream = new ObjectInputStream(fileStream); //Para leer objetos del fichero
 			
-		} catch (IOException e) { //Si hay alg˙n problema de entrada/salida (por ejemplo el fichero no existe devuelve una lista vacÌa)
+		} catch (IOException e) { //Si hay alg√∫n problema de entrada/salida (por ejemplo el fichero no existe devuelve una lista vac√≠a)
 			return lista_mezclas;
 		}
 		
 		try {
 			while (true) { // Se leen todos los objetos del fichero
 				Mezcla mezcla_aux = (Mezcla) objectStream.readObject(); // Se obtiene la mezcla del fichero
-				lista_mezclas.add(mezcla_aux); // Se aÒade a la lista
+				lista_mezclas.add(mezcla_aux); // Se a√±ade a la lista
 			}
-		} catch (EOFException e) { // Cuando salte la excepciÛn de fin de fichero
+		} catch (EOFException e) { // Cuando salte la excepci√≥n de fin de fichero
 			try {
 				objectStream.close(); // Se cierra el fichero
-				return lista_mezclas; // Devuelve la lista con las mezclas que leyÛ del fichero incluidas
+				return lista_mezclas; // Devuelve la lista con las mezclas que ley√≥ del fichero incluidas
 			} catch (IOException e1) {
 				return lista_mezclas;
 			}
@@ -70,7 +87,7 @@ public class MisMezclas extends ListActivity {
 		FileOutputStream fileStream;
 		ObjectOutputStream objectStream;
 		try { 
-	        fileStream = openFileOutput(getString(R.string.FILENAME_MEZCLAS), Context.MODE_PRIVATE); //Crea el fichero de mezclas en el directorio de fichero de la aplicaciÛn 
+	        fileStream = openFileOutput(getString(R.string.FILENAME_MEZCLAS), Context.MODE_PRIVATE); //Crea el fichero de mezclas en el directorio de fichero de la aplicaci√≥n 
 	        objectStream = new ObjectOutputStream(fileStream); 
 	    	for (Mezcla mezcla_aux : lista_mezclas) { //Recorre la lista escribiendo los objetos en el fichero
 	    		objectStream.writeObject(mezcla_aux);
@@ -86,7 +103,7 @@ public class MisMezclas extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mis_mezclas);
 		
-		lista_mezclas = leerFicheroMezclas(); //Obtiene la lista del fichero, si no existÌa tal fichero la lista estar· vacÌa
+		lista_mezclas = leerFicheroMezclas(); //Obtiene la lista del fichero, si no exist√≠a tal fichero la lista estar√° vac√≠a
 		listView = (ListView) findViewById(android.R.id.list); //Se obtiene el visor de listas
 		
 		listAdapter = new ArrayAdapter<Mezcla>(this, android.R.layout.simple_list_item_1, lista_mezclas); //Se crea un adaptador simple para la lista de mezclas		
@@ -94,25 +111,25 @@ public class MisMezclas extends ListActivity {
 	}
 	
 	@Override
-	protected void onStop() { //Se ejecutar· cada vez que la actividad pase a ser invisible
+	protected void onStop() { //Se ejecutar√° cada vez que la actividad pase a ser invisible
 		super.onStop();
 		
 		escribirFicheroMezclas(); //Actualiza el fichero de mezclas
 	}
 	
-	//Se ejecutar· al pulsar el boton "+" para aÒadir una mezcla
+	//Se ejecutar√° al pulsar el boton "+" para a√±adir una mezcla
 	public void mostrarNuevaMezcla(View view) {
 		Intent intent = new Intent(this, NuevaMezcla.class); //Se crea un "intent" con la actividad NuevaMezcla
 		startActivityForResult(intent, CODIGO_NUEVA); //Se muestra la actividad para crear una nueva mezcla y se esperan sus datos
 	}
 	
-	//Se ejecutar· al pulsar el botÛn de configuraciones (esquina superior izquierda)
+	//Se ejecutar√° al pulsar el bot√≥n de configuraciones (esquina superior izquierda)
 	public void mostrarPreferencias(View view) {
 		Intent intent = new Intent(this, Preferencias.class); //Se crea un "intent" con la actividad NuevaMezcla
 		startActivityForResult(intent, CODIGO_PREFERENCIAS); //Se muestra la actividad para crear una nueva mezcla y se esperan sus datos
 	}	
 	
-	//FunciÛn para leer el resultado de una actividad creada desde Èsta 
+	//Funci√≥n para leer el resultado de una actividad creada desde √©sta 
 	@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent datos) {
 		//super.onActivityResult(requestCode, resultCode, datos);
@@ -120,11 +137,11 @@ public class MisMezclas extends ListActivity {
 			if (requestCode == CODIGO_NUEVA) { //Si se trataba de la actividad para crear una nueva mezcla
 				Mezcla nueva_mezcla;
 				nueva_mezcla = (Mezcla) datos.getExtras().get(NuevaMezcla.EXTRA_MEZCLA_NUEVA); // Se obtiene la mezcla creada en la actividad							
-				lista_mezclas.add(nueva_mezcla); // Se aÒade a la lista de mezclas
+				lista_mezclas.add(nueva_mezcla); // Se a√±ade a la lista de mezclas
 //				((ArrayAdapter<Mezcla>) lv.getAdapter()).notifyDataSetChanged(); //Se notifica que la lista ha cambiado, debe refrescarse la vista
 			}
 			else if (requestCode == CODIGO_DETALLES) { //Si se trataba de la actividad para mostrar detalles de una mezcla
-				int posicion_aBorrar = datos.getExtras().getInt(DetallesMezcla.EXTRA_POSICION_MEZCLA_BORRADA); //Se obtiene la posiciÛn de la mezcla que se estaba mostrando, la cual se ha decidido borrar
+				int posicion_aBorrar = datos.getExtras().getInt(DetallesMezcla.EXTRA_POSICION_MEZCLA_BORRADA); //Se obtiene la posici√≥n de la mezcla que se estaba mostrando, la cual se ha decidido borrar
 				lista_mezclas.remove(posicion_aBorrar); //Se borra de la lista de mezclas
 //				((ArrayAdapter<Mezcla>) lv.getAdapter()).notifyDataSetChanged(); //Se notifica que la lista ha cambiado, debe refrescarse la vista
 			}
@@ -133,14 +150,14 @@ public class MisMezclas extends ListActivity {
 		}
 	}
 	
-	//Se ejecutar· al pulsar sobre un elemento de la lista
+	//Se ejecutar√° al pulsar sobre un elemento de la lista
 	@Override
 	protected void onListItemClick(ListView l, View v, int posicion, long id) {
 		 //super.onListItemClick(l, v, position, id);
 		Intent intent = new Intent(this, DetallesMezcla.class);
-		intent.putExtra(EXTRA_MEZCLA_LISTADA, lista_mezclas.get(posicion)); //Comunica la mezcla que se encuentra en la posiciÛn seleccionada
-		intent.putExtra(EXTRA_POSICION_MEZCLA_LISTADA, posicion); //TambiÈn comunica la posiciÛn que ocupa en la lista de mezclas
-		startActivityForResult(intent, CODIGO_DETALLES); //Se muestra la actividad para mostrar los detalles de la mezcla y se espera alg˙n resultado
+		intent.putExtra(EXTRA_MEZCLA_LISTADA, lista_mezclas.get(posicion)); //Comunica la mezcla que se encuentra en la posici√≥n seleccionada
+		intent.putExtra(EXTRA_POSICION_MEZCLA_LISTADA, posicion); //Tambi√©n comunica la posici√≥n que ocupa en la lista de mezclas
+		startActivityForResult(intent, CODIGO_DETALLES); //Se muestra la actividad para mostrar los detalles de la mezcla y se espera alg√∫n resultado
 	}	
 
 }
