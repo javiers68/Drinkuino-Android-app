31fd88bf82f75d1a5161ebd421f4e7e1fa706ab2
Pablo Toledo <jptgjuanpablo@gmail.com>
2014-11-13 20:43:12 +0100

    Update TextWatcherSinCero.java

--- Coctelera/src/dse/coctelera/TextWatcherSinCero.java
+++ Coctelera/src/dse/coctelera/TextWatcherSinCero.java
@@ -1,18 +1,35 @@
+
+/*
+ * Copyright (C) 2014 Drinkuino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package dse.coctelera;
 
 import android.text.Editable;
 import android.text.TextWatcher;
 
-//TextWatcher (vigilante de texto) para evitar que los campos de texto en donde se escribir·n valores tengan ceros como primer dÌgito
+//TextWatcher (vigilante de texto) para evitar que los campos de texto en donde se escribir√°n valores tengan ceros como primer d√≠gito
 public class TextWatcherSinCero implements TextWatcher {
 
 
 	public TextWatcherSinCero() {}
 	
-	//Se ejecutar· despuÈs de haberse realizado una escritura
+	//Se ejecutar√° despu√©s de haberse realizado una escritura
 	@Override
 	public void afterTextChanged(Editable editable) {
-		if ((editable.length() > 1) && (editable.charAt(0) == '0')) { //Si el primer car·cter escrito es un 0 y se escribieron varios n˙meros
+		if ((editable.length() > 1) && (editable.charAt(0) == '0')) { //Si el primer car√°cter escrito es un 0 y se escribieron varios n√∫meros
 			editable.replace(0, 1, ""); //Borra el 0 inicial
 		}
 	}
